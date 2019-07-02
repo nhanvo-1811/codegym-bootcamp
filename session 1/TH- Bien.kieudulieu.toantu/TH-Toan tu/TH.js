6259e8030c81eda28c8b48ed2856f6150fe7241a
shadowin1811 <shadowin1811@gmail.com>
2019-07-02 14:08:35 +0700

    .

---
+++ session 1/TH- Bien.kieudulieu.toantu/TH-Toan tu/TH.js
@@ -0,0 +1,8 @@
+let InputWidth
+let InputHeight
+InputHeight=prompt('Enter the width')
+InputWidth=prompt('Enter the height')
+let Width = parseInt(InputWidth)
+let Height = parseInt(InputHeight)
+let area = Width * Height
+document.write('The area is: ' + area)
