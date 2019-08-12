1f81a4c41363c6f6943afb2a6bf5ff32f889d8ff
nhanvo-1811 <52406746+nhanvo-1811@users.noreply.github.com>
2019-08-12 10:35:48 +0700

    .

---
+++ session 2/Bài Tập/BasicEx/BasicEx/Basic-W33/BT25.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicEx.Basic
+{
+    class BT25
+    {
+        static void Main()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
